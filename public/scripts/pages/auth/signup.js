05e373a0daa8cf41b50f3ae5f67a893e89712bb3
Forrest Collins <Forest.d.collins@gmail.com>
2017-03-12 17:42:04 -0500

    Refactored the old signup code, soon to change the way jwt's are handled

--- public/scripts/pages/auth/signup.js
+++ public/scripts/pages/auth/signup.js
@@ -1,121 +1,51 @@
 import axios from 'axios'
-import { cacheId } from '../../libs/utils'
 import notify from '../../components/notifications'
 
 export function signup() {
-	var cache = {};
-
-	let submitButton = cacheId(cache, 'signup-submit');
-	let email = cacheId(cache, 'signup-email');
-	let confirm = cacheId(cache, 'signup-confirm');
-	let password = cacheId(cache, 'signup-password');
-	let successContent = cacheId(cache, 'success-content');
-	let failureContent = cacheId(cache, 'failure-content');
-
-
-	let successNotify = new notify({
-		timeout: 1500,
-		content: successContent,
-		type: 'success'
-	});
-
-	let failureNotify = new notify({
-		timeout: 1500,
-		content: failureContent,
-		type: 'danger'
-	}); 
-
-	function validateEmail() {
-		let input = email.value;
-		let atpos = input.indexOf('@');
-		let dotpos = input.lastIndexOf(".");
-
-		if (atpos < 1 || (dotpos - atpos < 2)) {
-			if (email.parentNode.classList.contains('email-valid')) {
-				email.parentNode.classList.remove('email-valid');
-			}
-
-			email.parentNode.classList.add('email-invalid');
-		} else {
-			if (email.parentNode.classList.contains('email-invalid')) {
-				email.parentNode.classList.remove('email-invalid');
-			}
-
-			email.parentNode.classList.add('email-valid');
-		}
-	}
-
-	function validatePassword() {
-		let confirmValue = confirm.value;
-		let passwordValue = password.value;
-
-		if (confirmValue != passwordValue) {
-			if (confirm.parentNode.classList.contains('password-valid')) {
-				confirm.parentNode.classList.remove('password-valid')
-			}
-
-			confirm.parentNode.classList.add('password-invalid');
-		} else {
-			if (confirm.parentNode.classList.contains('password-invalid')) {
-				confirm.parentNode.classList.remove('password-invalid');
-			}
-
-			confirm.parentNode.classList.add('password-valid');
-		}
-	}
-
+	const name = document.getElementById('signup-name');
+	const email = document.getElementById('signup-email');
+	const password = document.getElementById('signup-password');
+	const confirm = document.getElementById('signup-password');
+	const submitButton = document.querySelector('.auth-form-submit button');
 
 	function submit() {
-		submitButton.classList.add('show-loading');
-			
-		let data = {};
-		data.name = cacheId(cache, 'signup-name');
-		data.username = cacheId(cache, 'signup-username');
-		data.email = email;
-		data.password = password;
+		if (submitButton.classList.contains('form-valid')) {
+			submitButton.classList.add('form-loading');
 
-		axios.post('http://localhost:3000/users', {
-			name: data.name.value,
-			username: data.username.value,
-			email: data.email.value,
-			password: data.password.value,
+			axios.post('http://localhost:8000/signup', {
+				name: name.value,
+				email: email.value,
+				password: password.value,
 
-			headers: {
-				'Content-Type': 'application/json'
-			}
-		})
-		.then((response) => {
-			submitButton.classList.remove("show-loading");
-			
-			if (response.data.success) {
-				submitButton.classList.add('submit-success');
-				var success = new Event('signed-up');
-				window.dispatchEvent(success);
-				removeEvents();
-				window.localStorage.setItem('user', user);
-				window.localStorage.setItem('blog-token', response.data.res.token);
-				
-				setTimeout(() => {
-					window.location.href = '/'
-				}, 500);					
+				headers: {
+					'Content-Type': 'Application/JSON'
+				}
+			})
+			.then((response) => {
+				submitButton.classList.remove('form-loading');
 
-			} else {
-				submitButton.classList.add('submit-failed');
-				var failure = new Event('signup-fail');
-				window.dispatchEvent(failure);
-			}
-		})			
-	} 
+				if (response.data.success) {
+					submitButton.classList.add('form-success');
 
-	function removeEvents() {
-		submitButton.removeEventListener('click', submit);
-		email.removeEventListener('blur', validateEmail);
-		email.removeEventListener('blur', validatePassword);
-	} 
+					const user = JSON.stringify(response.data.res.record);
+					const success = new Event('signup-success');
 
-	email.addEventListener('blur', validateEmail);
-	confirm.addEventListener('blur', validatePassword);
-	submitButton.addEventListener('click', submit);
-	window.addEventListener('signed-up', successNotify.open);
-	window.addEventListener('signup-fail', failureNotify.open); 
+					window.localStorage.setItem('user', user);
+					window.localStorage.setItem('blog-token', response.data.res.token);
+
+					window.dispatchEvent(success);
+				} else {
+					submitButton.classList.add('form-failure');
+
+					const failure = new Event('signup-failure');
+
+					window.dispatchEvent(failure);
+				}
+			})
+		} else {
+			const error = new Event('signup-error');
+
+			window.dispatchEvent(error);
+		}
+	}
 }
